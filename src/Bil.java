1c44d82befd22c13a1475921a47d37678f7d1d08
jjuliamolin <julia.molin@mail.com>
2019-11-14 13:23:25 +0100

    hejhej

--- src/Bil.java
+++ src/Bil.java
@@ -42,6 +42,7 @@ public abstract class Bil {
         public int getNrDoors(){
             return nrDoors;
         }
+        //hej
 
         /**
          *
--- src/Volvo.java
+++ src/Volvo.java
@@ -1,11 +1,6 @@
 import java.awt.*;
 
 public class Volvo extends Bil implements Movable{
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
 
     /**
      * Constructs a Volvo which extends Bil.
